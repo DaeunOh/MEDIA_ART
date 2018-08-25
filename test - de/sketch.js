a0daf39079e57d3449a57ebee99e45ff2be086fa
DaeunOh <d30902430@gmail.com>
2018-08-26 04:56:18 +0900

    하트 터지는 효과

--- test - de/sketch.js
+++ test - de/sketch.js
@@ -1,51 +1,101 @@
 var sound;
 var fft;
 var sumOfWeight = 0;
-var hearts = [];
 var index = 0;
+
+var hearts = [];
+var smallHearts = [];
+var heartIndex = 0;
 
 var dx = 200;
 var dy = 200;
+var smallHeartX;
+var smallHeartY;
+
+var locked = false;
+var prevFrameCount = 0;
 
 var mic = null;
 
 class heart {
-    constructor(x, y, size ,alpha){
+    constructor(x, y, size, alpha) {
         this.x = x;
         this.y = y;
         this.size = size;
         this.alpha = alpha;
     }
-    heartDraw(){
+
+    heartDraw() {
         noStroke();
         beginShape();
-        curveVertex(this.x,this.y-this.size-this.size/8);
-        curveVertex(this.x,this.y-this.size-this.size/8);
+        curveVertex(this.x, this.y - this.size - this.size / 8);
+        curveVertex(this.x, this.y - this.size - this.size / 8);
 
-        curveVertex(this.x-this.size/2-this.size/8,this.y-2*this.size+this.size/8);
-        curveVertex(this.x-this.size/2-this.size-this.size/8,this.y-2*this.size+this.size/8);
+        curveVertex(this.x - this.size / 2 - this.size / 8, this.y - 2 * this.size + this.size / 8);
+        curveVertex(this.x - this.size / 2 - this.size - this.size / 8, this.y - 2 * this.size + this.size / 8);
 
-        curveVertex(this.x-2*this.size-this.size/4,this.y-this.size);
-        curveVertex(this.x-2*this.size-this.size/4,this.y);
-        curveVertex(this.x-this.size-this.size/2,this.y+this.size);
+        curveVertex(this.x - 2 * this.size - this.size / 4, this.y - this.size);
+        curveVertex(this.x - 2 * this.size - this.size / 4, this.y);
+        curveVertex(this.x - this.size - this.size / 2, this.y + this.size);
 
-        vertex(this.x,this.y+2*this.size);
-        vertex(this.x,this.y+2*this.size);
-        vertex(this.x,this.y+2*this.size);
+        vertex(this.x, this.y + 2 * this.size);
+        vertex(this.x, this.y + 2 * this.size);
+        vertex(this.x, this.y + 2 * this.size);
 
-        curveVertex(this.x+this.size+this.size/2,this.y+this.size);
+        curveVertex(this.x + this.size + this.size / 2, this.y + this.size);
 
-        curveVertex(this.x+2*this.size+this.size/4,this.y);
-        curveVertex(this.x+2*this.size+this.size/4,this.y-this.size);
+        curveVertex(this.x + 2 * this.size + this.size / 4, this.y);
+        curveVertex(this.x + 2 * this.size + this.size / 4, this.y - this.size);
 
-        curveVertex(this.x+this.size*13/8,this.y-2*this.size+this.size/8);
-        curveVertex(this.x+this.size*5/8,this.y-2*this.size+this.size/8);
+        curveVertex(this.x + this.size * 13 / 8, this.y - 2 * this.size + this.size / 8);
+        curveVertex(this.x + this.size * 5 / 8, this.y - 2 * this.size + this.size / 8);
         endShape(CLOSE);
     }
-    heartSet(dx, dy){
+
+    heartSet(dx, dy) {
         this.dx = dx;
         this.dy = dy;
     }
+}
+
+function generateSmallHeart(index, size) {
+    return {
+        x: Math.random() * 10 + smallHeartX,
+        y: Math.random() * 10 + smallHeartY,
+        size: size,
+        h: index,
+        v: {
+            x: Math.random() * 10 - 5,
+            y: Math.random() * 10 - 5,
+        }
+    }
+}
+
+function smallHeart(x, y, size) {
+    noStroke();
+    beginShape();
+    curveVertex(x, y - size - size / 8);
+    curveVertex(x, y - size - size / 8);
+
+    curveVertex(x - size / 2 - size / 8, y - 2 * size + size / 8);
+    curveVertex(x - size / 2 - size - size / 8, y - 2 * size + size / 8);
+
+    curveVertex(x - 2 * size - size / 4, y - size);
+    curveVertex(x - 2 * size - size / 4, y);
+    curveVertex(x - size - size / 2, y + size);
+
+    vertex(x, y + 2 * size);
+    vertex(x, y + 2 * size);
+    vertex(x, y + 2 * size);
+
+    curveVertex(x + size + size / 2, y + size);
+
+    curveVertex(x + 2 * size + size / 4, y);
+    curveVertex(x + 2 * size + size / 4, y - size);
+
+    curveVertex(x + size * 13 / 8, y - 2 * size + size / 8);
+    curveVertex(x + size * 5 / 8, y - 2 * size + size / 8);
+    endShape(CLOSE);
 }
 
 function preload() {
@@ -53,7 +103,7 @@ function preload() {
 }
 
 function setup() {
-    var cnv = createCanvas(1800, 1000);
+    var cnv = createCanvas(1024, 800);
     cnv.mouseClicked(togglePlay);
     fft = new p5.FFT(0, 256);
     sound.amp(0.5);
@@ -61,6 +111,10 @@ function setup() {
 
     mic = new p5.AudioIn();
     mic.start();
+
+    for (var i = 0; i < 360; i++) {
+        smallHearts[i] = generateSmallHeart(i, 3);
+    }
 }
 
 function draw() {
@@ -72,7 +126,7 @@ function draw() {
 
     var spectrum = fft.analyze();
 
-    noStroke();
+    //noStroke();
     //fill(0,255,0); // spectrum is green
     //console.log(spectrum.length);
     sumOfWeight = 0;
@@ -87,13 +141,13 @@ function draw() {
         else if (spectrum[i] >= 72 && spectrum[i] < 108) {
             sumOfWeight += 30;
         }
-        else if(spectrum[i] >= 108 && spectrum[i] < 145) {
+        else if (spectrum[i] >= 108 && spectrum[i] < 145) {
             sumOfWeight += 40;
         }
-        else if(spectrum[i] >= 145 && spectrum[i] < 182) {
+        else if (spectrum[i] >= 145 && spectrum[i] < 182) {
             sumOfWeight += 50;
         }
-        else if(spectrum[i] >= 182 && spectrum[i] < 219) {
+        else if (spectrum[i] >= 182 && spectrum[i] < 219) {
             sumOfWeight += 60;
         }
         else {
@@ -101,7 +155,7 @@ function draw() {
         }
     }
 
-    for(var i = 0; i<hearts.length; i++){
+    for (var i = 0; i < hearts.length; i++) {
         hearts[i].size += 0.2;
         hearts[i].alpha -= 3;
 
@@ -109,20 +163,57 @@ function draw() {
         hearts[i].heartDraw();
     }
 
-    // var generateCount = Math.floor(mic.getLevel() * 1000);
+    for (var i = 0; i < smallHearts.length; i++) {
+        var h = smallHearts[i];
 
+        stroke(0);
+        strokeWeight(0);
 
-    if(mic.getLevel() * 1000 > 100) {
-        var tempX = random(100,width-100);
-        dx = tempX;
-        var tempY = random(100,height-100);
-        dy = tempY;
+        h.x += h.v.x;
+        h.y += h.v.y;
+        h.v.x += Math.random() - 0.5;
+        h.v.y += Math.random() - 0.5;
+        h.size -= 0.1;
+
+        fill(map(sumOfWeight, 5000, 10000, 0, 255), 200, 200, h.size * 20);
+        smallHeart(h.x, h.y, h.size);
     }
 
-    if(frameCount % 60 == 0) {
-        hearts[index] = new heart(dx,dy,14,252);
+    var generateCount = Math.floor(mic.getLevel() * 1000);
+    if (generateCount > 100) { // clap
+        if (!locked) {
+            locked = true;
 
-        if(index == 20)
+            prevFrameCount = frameCount;
+
+            smallHeartX = dx;
+            smallHeartY = dy;
+
+            for (var i = 0; i < generateCount - 50; i++) {
+                heartIndex++;
+                heartIndex %= smallHearts.length;
+                smallHearts[heartIndex] =
+                    generateSmallHeart(heartIndex, 8);
+            }
+
+            var tempX = random(100, width - 100);
+            dx = tempX;
+            var tempY = random(100, height - 100);
+            dy = tempY;
+        }
+        else {
+            if (frameCount - prevFrameCount > 10) {
+                locked = false;
+            }
+        }
+
+
+    }
+
+    if (frameCount % 60 == 0) { // 20(fast) ~ 60(slow)
+        hearts[index] = new heart(dx, dy, 14, 252);
+
+        if (index == 20)
             index = 0;
         else
             index++;
