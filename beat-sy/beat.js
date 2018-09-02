4b751a3e5a544b77fee5ae28cb9755d4b4999039
Park Sin Young <qkrtlsdud428@gmail.com>
2018-09-02 20:49:14 +0900

    beatheart

--- beat-sy/beat.js
+++ beat-sy/beat.js
@@ -2,12 +2,23 @@ var sound;
 var fft;
 var sumOfWeight = 0;
 var hearts = [];
-var smallhearts = [];
+var beatHearts = [];
 var index = 0;
 var si = 0;
 
+var smallHearts = [];
+var heartIndex = 0;
+var locked = false;
+var prevFrameCount = 0;
+
 var dx = 200;
 var dy = 200;
+
+
+var r = 120;
+var hs = 7;
+var smallHeartX = 200;
+var smallHeartY = 200;
 
 var mic = null;
 
@@ -17,7 +28,7 @@ var backgroundColor;
 
 var heartRotate = true;
 
-var beatHoldFrames = 200;
+var beatHoldFrames = 30;
 var beatThreshold = 0.11;
 
 var beatCutoff = 0;
@@ -59,14 +70,50 @@ class heart {
         endShape(CLOSE);
     }
 
-    heartSet(dx, dy) {
-        this.dx = dx;
-        this.dy = dy;
+}
+
+function generateSmallHeart(index, size, smallHeartX, smallHeartY) {
+    return {
+        x: Math.random() * 10 + smallHeartX,
+        y: Math.random() * 10 + smallHeartY,
+        size: size,
+        h: index,
+        v: {
+            x: Math.random() * 10 - 5,
+            y: Math.random() * 10 - 5,
+        }
     }
 }
 
+function smallHeart(x, y, size) {
+    noStroke();
+    beginShape();
+    curveVertex(x, y - size - size / 8);
+    curveVertex(x, y - size - size / 8);
+
+    curveVertex(x - size / 2 - size / 8, y - 2 * size + size / 8);
+    curveVertex(x - size / 2 - size - size / 8, y - 2 * size + size / 8);
+
+    curveVertex(x - 2 * size - size / 4, y - size);
+    curveVertex(x - 2 * size - size / 4, y);
+    curveVertex(x - size - size / 2, y + size);
+
+    vertex(x, y + 2 * size);
+    vertex(x, y + 2 * size);
+    vertex(x, y + 2 * size);
+
+    curveVertex(x + size + size / 2, y + size);
+
+    curveVertex(x + 2 * size + size / 4, y);
+    curveVertex(x + 2 * size + size / 4, y - size);
+
+    curveVertex(x + size * 13 / 8, y - 2 * size + size / 8);
+    curveVertex(x + size * 5 / 8, y - 2 * size + size / 8);
+    endShape(CLOSE);
+}
+
 function preload() {
-    sound = loadSound("blackpink.mp3");
+    sound = loadSound("iu.mp3");
 }
 
 function setup() {
@@ -90,6 +137,12 @@ function setup() {
 
     amplitude.setInput(sound);
     amplitude.smooth(0.9);
+
+    beatHearts[0] = new heart(dx, dy + r, 5, 252);
+
+    for (var i = 0; i < 360; i++) {
+        smallHearts[i] = generateSmallHeart(i, 12, smallHeartX, smallHeartY);
+    }
 }
 
 function draw() {
@@ -108,6 +161,7 @@ function draw() {
     //fill(0,255,0); // spectrum is green
     //console.log(spectrum.length);
     sumOfWeight = 0;
+
 
     for (var i = 0; i < spectrum.length; i++) {
         if (spectrum[i] >= 0 && spectrum[i] < 36) {
@@ -140,39 +194,109 @@ function draw() {
         var rotation = 0;
     }
 
+
     for (var i = 0; i < hearts.length; i++) {
         hearts[i].size += 0.2;
-        //hearts[i].size += distortDiam/1000;
         hearts[i].alpha -= 3;
-        //rotate(rotation);
 
         fill(map(sumOfWeight, 5000, 10000, 0, 255) % 256, 255, 255, hearts[i].alpha);
         hearts[i].heartDraw();
-
-    }
-    for (var i = 0; i < smallhearts.length; i++){
-        smallhearts[i].heartDraw();
     }
 
+
+    for (var i = 0; i < beatHearts.length; i++) {
+        beatHearts[i].alpha -= 1.5;
+
+        fill(map(sumOfWeight, 5000, 10000, 0, 255) % 256, 255, 255, beatHearts[i].alpha);
+        beatHearts[i].heartDraw();
+    }
 
     // var generateCount = Math.floor(mic.getLevel() * 1000);
+    for (var i = 0; i < smallHearts.length; i++) {
+        var h = smallHearts[i];
+
+        stroke(0);
+        strokeWeight(0);
+
+        h.x += h.v.x;
+        h.y += h.v.y;
+        h.v.x += Math.random() - 0.5;
+        h.v.y += Math.random() - 0.5;
+        h.size -= 0.1;
+
+        fill(map(sumOfWeight, 5000, 10000, 0, 255), 200, 200, h.size * 20);
+        smallHeart(h.x, h.y, h.size);
+    }
+
+    var generateCount = Math.floor(mic.getLevel() * 1000);
+    if (generateCount > 100) { // clap
+        if (!locked) {
+            locked = true;
+
+            prevFrameCount = frameCount;
+
+            smallHeartX = dx;
+            smallHeartY = dy;
 
 
-    if (mic.getLevel() * 1000 > 100) {
-        var tempX = random(100, width - 100);
-        dx = tempX;
-        var tempY = random(100, height - 100);
-        dy = tempY;
+            for (var i = 0; i < generateCount - 50; i++) {
+                heartIndex++;
+                heartIndex %= smallHearts.length;
+                smallHearts[heartIndex] =
+                    generateSmallHeart(heartIndex, 12, smallHeartX, smallHeartY);
+            }
+
+            var tempX = random(100, width - 150);
+            dx = tempX;
+            var tempY = random(100, height - 150);
+            dy = tempY;
+        }
+        else {
+            if (frameCount - prevFrameCount > 10) {
+                locked = false;
+            }
+        }
+
+
     }
 
     if (frameCount % 60 == 0) {
-        hearts[index] = new heart(dx, dy, 14, 252);
-
+        hearts[index] = new heart(dx, dy, 18, 252);
         if (index == 20)
             index = 0;
         else
             index++;
     }
+    //
+    //
+    // if (mic.getLevel() * 1000 > 100) {
+    //     var tempX = random(100, width - 100);
+    //     dx = tempX;
+    //     tempX = random(100, width - 100);
+    //     dx2 = tempX;
+    //     tempX = random(100, width - 100);
+    //     dx3 = tempX;
+    //
+    //     var tempY = random(100, height - 100);
+    //     dy = tempY;
+    //     tempY = random(100, height - 100);
+    //     dy2 = tempY;
+    //     tempY = random(100, height - 100);
+    //     dy3 = tempY;
+    //
+    // }
+
+    // if (frameCount % 60 == 0) {
+    //     hearts[index] = new heart(dx, dy, 14, 252);
+    //     hearts2[index] = new heart(dx2, dy2, 14, 252);
+    //     hearts3[index] = new heart(dx3, dy3, 14, 252);
+    //
+    //     if (index == 20)
+    //         index = 0;
+    //     else
+    //         index++;
+    // }
+
 
     // for (var i = 0; i < spectrum.length; i++) {
     //     // console.log("second" + i);
@@ -200,6 +324,7 @@ function draw() {
     text('click to play/pause', 4, 10);
 }
 
+
 // fade sound if mouse is over canvas
 function togglePlay() {
     if (sound.isPlaying()) {
@@ -226,18 +351,43 @@ function detectBeat(level) {
 }
 
 function onBeat() {
-    hearts[si] = new heart(dx + random(-40, 40), dy + random(-40, 40), 2, 252);
+    if (si === 0) {
+        beatHearts[0] = new heart(dx, dy + r, hs, 252);
+    }
+    else if (si === 1) {
+        beatHearts[1] = new heart(dx + r / sqrt(2), dy + r / sqrt(2), hs, 252);
+    }
+    else if (si === 2) {
+        beatHearts[2] = new heart(dx + r, dy, hs, 252);
+    }
+    else if (si === 3) {
+        beatHearts[3] = new heart(dx + r / sqrt(2), dy - r / sqrt(2), hs, 252);
+    }
+    else if (si === 4) {
+        beatHearts[4] = new heart(dx, dy - r, hs, 252);
+    }
+    else if (si === 5) {
+        beatHearts[5] = new heart(dx - r / sqrt(2), dy - r / sqrt(2), hs, 252);
+    }
+    else if (si === 6) {
+        beatHearts[6] = new heart(dx - r, dy, hs, 252);
+    }
+    else if (si === 7) {
+        beatHearts[7] = new heart(dx - r / sqrt(2), dy + r / sqrt(2), hs, 252);
+    }
 
-    if (si == 6)
+    if (si == 7)
         si = 0;
     else
         si++;
+
+    console.log(si);
 
     heartRotate = !heartRotate;
     backgroundColor = color(random(0, 255), random(0, 255), random(0, 255));
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-    background(0);
-}
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+//     background(0);
+// }
